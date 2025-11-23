62b34b9686233cb8087030d055b7611a1329b8ea
Asusena Ela Öztürk <119732494+Aseloz@users.noreply.github.com>
2025-11-23 21:03:19 +0300

    update set_page_config.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+
+
+st.set_page_config(
+    page_title="Netflix Data Visualization Dashboard",
+    layout="wide"
+)
